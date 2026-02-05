a85455b53922b01da37b0c7f31865df338478d6e
randShuffle <k06126@126.com>
2026-02-05 20:27:55 +0800

    fix

--- pipeline/field_charcnn_phishintention.sh
+++ pipeline/field_charcnn_phishintention.sh
@@ -1,7 +1,7 @@
 while true
 do
   python ./model_update.py --exp_name field_charcnn_phishintention
-  ps -u zhuocheng -o pid,cmd | grep model_consumer.py | grep field_charcnn_phishintention | grep -v grep | awk '{print $1}' | xargs -r kill -9
+  ps -u $USER -o pid,cmd | grep model_consumer.py | grep field_charcnn_phishintention | grep -v grep | awk '{print $1}' | xargs -r kill -9
   echo "[$(date '+%Y-%m-%d %H:%M:%S')] Pipeline Ready to restart..."
   nohup python ./model_consumer.py --exp_name field_charcnn_phishintention >/dev/null 2>&1 &
   # retrain every 2 hours
